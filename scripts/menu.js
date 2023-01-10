8dd924e207145a512f4b7b9b695cbc338c0b5600
hal22036 <jeni.lee.hales@gmail.com>
2023-01-10 11:58:03 -0800

    dark mode contrast

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,21 +1,10 @@
 const menubutton = document.querySelector("#menu-button");
 const menuitems = document.querySelectorAll(".menu-item");
 const div = document.querySelector('#hamb-menu');
-// let isClicked = true;
 
-// let showHide = function (){
-// 	if (isClicked){
-// 		div.style.display = 'block';
-// 		isClicked = false;
-// 	} else{
-// 		div.style.display = 'none';
-// 		isClicked = true;
-// }
-// }
 
 menubutton.addEventListener("click", () => {
 	menuitems.forEach((item) => item.classList.toggle("open"));
-	div.style.display = 'block';
+	div.style.display = 'flex';
 	menubutton.classList.toggle("close");
-	div.style.display.toggle('none');
 });
--- scripts/mode.js
+++ scripts/mode.js
@@ -2,15 +2,18 @@ const modeButton = document.querySelector('#darkModeIcon');
 let light = true;
 const bodyElement = document.querySelector('body');
 const charColor = document.querySelectorAll('p, a, h2');
+const charMenu = document.querySelectorAll('.menu-item');
 
 modeButton.addEventListener('click', () => {
     if (light) {
         bodyElement.style.background = '#5F5F60';
         charColor.forEach((item) => item.classList.toggle('darkP'));
+        charMenu.forEach((item) => item.classList.toggle('darkMenu'));
         light = false;
     } else if(!light) {
         bodyElement.style.background = '#fbf7ef';
         charColor.forEach((item) => item.classList.toggle('darkP'));
+        charMenu.forEach((item) => item.classList.toggle('darkMenu'));
         light = true;
     }
 });
