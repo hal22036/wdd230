3f16ffcaa2fa8a8010e53145c8156da186762691
hal22036 <jeni.lee.hales@gmail.com>
2023-01-13 18:41:32 -0800

    home page COC

---
+++ chamber/scripts/chamber-dates.js
@@ -0,0 +1,5 @@
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+const date = `&copy; ${new Date().getFullYear()} Jeni Hales, WDD 230 Project`;
+document.getElementById('copyright').innerHTML = date;
+
---
+++ chamber/scripts/chamber-menu.js
@@ -0,0 +1,18 @@
+const menubutton = document.querySelector("#menu-button");
+const menuitems = document.querySelectorAll(".menu-item");
+const div = document.querySelector('#hamb-menu');
+var closeMenu = true;
+
+
+menubutton.addEventListener("click", () => {
+	if (closeMenu) {
+		menubutton.src = 'images/x.png';
+		closeMenu = false;
+	} else {
+		menubutton.src = "images/white-menu.png";
+		closeMenu = true;
+	}
+	menuitems.forEach((item) => item.classList.toggle("open"));
+	div.style.display = 'flex';
+	menubutton.classList.toggle("close");
+});
