07e1684fc827ea1bc8f8741306b023b243f7f569
hal22036 <jeni.lee.hales@gmail.com>
2023-01-20 08:50:38 -0800

    update header chamber

--- chamber/scripts/chamber-dates.js
+++ chamber/scripts/chamber-dates.js
@@ -3,3 +3,8 @@ document.getElementById("lastModified").innerHTML = `Last Modified: ${document.l
 const date = `&copy; ${new Date().getFullYear()} Jeni Hales, WDD 230 Project`;
 document.getElementById('copyright').innerHTML = date;
 
+const now = new Date().toLocaleDateString('en-us', {weeday:'long', year:'numeric', month:'long', day:'numeric'});
+document.getElementById("today").innerHTML = `Current Date: ${now}`;
+
+const time = new Date().toLocaleTimeString();
+document.getElementById("time").innerHTML = `Local Time: ${time}`;
