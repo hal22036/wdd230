68f02304b524b2dc2aef2fcfe0b3982d5ad60e05
hal22036 <jeni.lee.hales@gmail.com>
2023-01-21 08:37:31 -0800

    update js

--- chamber/scripts/chamber-visits.js
+++ chamber/scripts/chamber-visits.js
@@ -1,27 +1,26 @@
 const visitors = document.querySelector('.visitors');
-let start = Date.now();
-let end = localstorage.getItem.Date();
-let daysSinceLastVisit = getNumberOfDays(start, end);
 
 function getNumberOfDays(start, end) {
 	const date1 = new Date(start);
 	const date2 = new Date(end);
 
 	const oneDay = 1000 *60 * 60 * 24;
-	const diffInTime = date2.getTime() - date1.getTime();
-	const diffInDays = Math.round(diffInTime / oneDay);
+	const diffInTime = date1.getTime() - date2.getTime();
+	const diffInDays = Math.floor(diffInTime / oneDay);
 	return diffInDays;
 }
+let start = new Date();
+let end = localStorage.getItem('last-visit');
 
-if (daysSinceLastVisit !== 0) {
+
+
+if (end !== null) {
+	let daysSinceLastVisit = getNumberOfDays(start, end);
 	visitors.textContent = `Days Since Last Visit: ${daysSinceLastVisit}`;
 }else {
 	visitors.textContent = 'This is your first visit!';
 }
 
+localStorage.setItem('last-visit', start);
 
-
-localStorage.setItem('visits-days', daysSinceLastVisit);
-visitors.textContent = Date.now()
-
-console.log(getNumberOfDays(1/1/2023, 1/20/2023))
+// console.log(getNumberOfDays("1/20/2023", "1/1/2023"))
