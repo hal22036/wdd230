e883a4a281122b58ee8c1829c05e8596625a0e09
hal22036 <jeni.lee.hales@gmail.com>
2023-01-27 11:42:18 -0800

    fix header logo

--- chamber/scripts/chamber-dates.js
+++ chamber/scripts/chamber-dates.js
@@ -4,9 +4,7 @@ const date = `&copy; ${new Date().getFullYear()} Jeni Hales, WDD 230 Project`;
 document.getElementById('copyright').innerHTML = date;
 
 const now = new Date().toLocaleDateString('en-us', {weeday:'long', year:'numeric', month:'long', day:'numeric'});
-document.getElementById("today").innerHTML = `Current Date: ${now}`;
-
 const time = new Date().toLocaleTimeString();
-document.getElementById("time").innerHTML = `Local Time: ${time}`;
+document.getElementById("today").innerHTML = `Current Date & Time: ${now} ${time}`;
 
 const hidden = document.getElementById("hidden").value = `Date & Time: ${now}, ${time}`;
