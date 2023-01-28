d5d4797835f732ba8ce1edbf10946b8b655eae6b
hal22036 <jeni.lee.hales@gmail.com>
2023-01-28 08:44:47 -0800

    fix java

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,4 +1,4 @@
-const now = new Date().toLocaleDateString('en-us', {weeday:'long', year:'numeric', month:'long', day:'numeric'});
-const time = new Date().toLocaleTimeString();
+const joinNow = new Date().toLocaleDateString('en-us', {weeday:'long', year:'numeric', month:'long', day:'numeric'});
+const joinTime = new Date().toLocaleTimeString();
 const hidden = document.getElementById("hidden");
-hidden.value = `Date & Time: ${now} ${time}`;
+hidden.value = `Date & Time: ${joinNow} ${joinTime}`;
