daec27243cbfb00c8c07b3e90791442359371584
hal22036 <jeni.lee.hales@gmail.com>
2023-02-01 09:52:04 -0800

    learning activities

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -7,7 +7,7 @@ async function getDirectory() {
         if (response.ok) {
           const data = await response.json();
           console.log(data); 
-        //   displayMembers(data); 
+          displayMembers(data); 
         } else {
             throw Error(await response.text());
         }
@@ -15,11 +15,41 @@ async function getDirectory() {
           console.log(error);
       }
 };
-
-displayMembers();
+getDirectory();
 
 function displayMembers(data) {
-    data.forEach((element) => {
+    data.companies.forEach((element) => {
         console.log(element);
+        let member = document.createElement('section');
+        let company = document.createElement('h2');
+        let address = document.createElement('ul');
+        let street = document.createElement('li');
+        let city = document.createElement('li');
+        let phone = document.createElement('li');
+        let url = document.createElement('li');
+        let membership = document.createElement('li');        
+        let logo = document.createElement('img');
+
+        company.textContent = element.name; 
+        street.textContent = element.address[0].street;
+        city.textContent = `${element.address[0].city}, ${element.address[0].state} ${element.address[0].zip}`;
+        phone.textContent = `Phone # ${element.phone}`;
+        url.textContent = element.website;
+        membership.textContent = element.membership;
+        logo.setAttribute('src', element.image);
+        logo.setAttribute('alt', element.name);
+        // logo.setAttribute('loading', 'lazy');
+        // logo.setAttribute('width', '340');
+        // logo.setAttribute('height', '440');
+
+        member.appendChild(company);
+        member.appendChild(address);
+        member.appendChild(street);
+        member.appendChild(city);
+        member.appendChild(phone);
+        member.appendChild(url);
+        member.appendChild(membership);
+        member.appendChild(logo);
+        gridView.appendChild(member);
     });
 }
--- scripts/learning.js
+++ scripts/learning.js
@@ -1,62 +1,37 @@
-const learningCard = document.querySelector('#learning');
+const learning = document.querySelector('#learning');
 const learningFile = 'data/learning.json';
+let ul = document.createElement('ul');
+
+getAssignments();
+
+function displayActivity(data) {        
+    data.assignments.forEach((element) => {
+        let li = document.createElement('li');
+        let p = document.createElement('p');
+        p.textContent = `${element.label}:`;
+        element.links.forEach((link) => {
+            let a = document.createElement('a');            
+            a.textContent = link.tag;
+            a.setAttribute('href', link.href);
+            p.appendChild(a);  
+        })
+        li.appendChild(p);
+        ul.appendChild(li);
+        });
+    learning.appendChild(ul);
+}
 
 async function getAssignments() {
     try {
         const response = await fetch(learningFile);
         if (response.ok) {
           const data = await response.json();
-          console.log(data); 
-          getAssignments(data); 
+        //   console.log(data); 
+          displayActivity(data); 
         } else {
             throw Error(await response.text());
         }
       } catch (error) {
           console.log(error);
       }
-};
-
-getAssignments();
-
-function displayActivity(data) {    
-    let card = document.createElement('ul');
-    data.forEach((element) => {
-    // console.log(element);
-    let week = document.createElement('li');
-    let weekN = document.createElement('p');
-    let assignmentName = document.createElement('a');
-
-    week.textContent = data.weeks[i];
-    weekN.textContent = data.
-
-    });
-};
-// const displayActivity = (activity) => {
-//     activity.forEach ((activity) => {
-//         let week = document.createElement('li');
-//         let activities = document.createElement('p');
-//         let assignment = document.querySelector('a');
-
-//         week.textContent = activity;
-//         activities.textContent = activity.week;
-//         assignment.textContent = activity.activities[i];
-
-//         assignment.setAttribute('href', assignment[i]);
-
-//         activity.appendChild(week);
-//         activity.appendChild(activities);
-//         activity.appendChild(assignment);
-//         learningCard.appendChild(activity);
-//     });
-// }
-
-
-
-
-// {
-//     "week01": ["week01/holygrail.html", "week01/media-query.html", "https://codepen.io/hal22036/pen/OJwXZjK", "week01/chamber-site-plan.html"],
-//     "week02": ["week02/design.html", "https://codepen.io/hal22036/pen/dyjOrvo", "week02/bom.html"],
-//     "week03": ["week03/images/landscape.webp", "week03/lazyload.html", "https://codepen.io/hal22036/pen/bGjrPVz?editors=1000", "week03/bom.html"],
-//     "week04": ["https://codepen.io/hal22036/pen/ZEjrVQE?editors=1100", "week04/images/combinators.png", "week04/tablebuild.html", "week04/form-start/index.html"],
-//     "week05": ["https://codepen.io/hal22036/pen/mdjGVKE", "week05/new-ward-members.json", "week05/prophets.html", "week05/weather.html"]
-//   }
+}
