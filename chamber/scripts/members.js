731299065abecc16831209eb8fef0b732f867d6f
hal22036 <jeni.lee.hales@gmail.com>
2023-02-01 18:46:27 -0800

    directory page

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,13 +1,13 @@
 const gridView = document.querySelector('.dir-grid');
 const directory = 'data/members.json';
 
-async function getDirectory() {
+async function getDirectory(viewType) {
     try {
         const response = await fetch(directory);
         if (response.ok) {
           const data = await response.json();
-          console.log(data); 
-          displayMembers(data); 
+        //   console.log(data); 
+          viewType(data); 
         } else {
             throw Error(await response.text());
         }
@@ -15,11 +15,11 @@ async function getDirectory() {
           console.log(error);
       }
 };
-getDirectory();
+getDirectory(displayGridMembers);
 
-function displayMembers(data) {
+function displayGridMembers(data) {
     data.companies.forEach((element) => {
-        console.log(element);
+        // console.log(element);
         let member = document.createElement('section');
         let company = document.createElement('h2');
         let address = document.createElement('ul');
@@ -35,21 +35,91 @@ function displayMembers(data) {
         city.textContent = `${element.address[0].city}, ${element.address[0].state} ${element.address[0].zip}`;
         phone.textContent = `Phone # ${element.phone}`;
         url.textContent = element.website;
-        membership.textContent = element.membership;
+        membership.textContent = `Membership: ${element.membership}`;
         logo.setAttribute('src', element.image);
         logo.setAttribute('alt', element.name);
-        // logo.setAttribute('loading', 'lazy');
-        // logo.setAttribute('width', '340');
-        // logo.setAttribute('height', '440');
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '40%');
+        logo.setAttribute('height', 'auto');
 
         member.appendChild(company);
-        member.appendChild(address);
-        member.appendChild(street);
-        member.appendChild(city);
-        member.appendChild(phone);
-        member.appendChild(url);
-        member.appendChild(membership);
         member.appendChild(logo);
+        address.appendChild(street);
+        address.appendChild(city);
+        address.appendChild(phone);
+        address.appendChild(url);
+        address.appendChild(membership);
+        member.appendChild(address);
         gridView.appendChild(member);
     });
 }
+
+function displayListMembers(data) {    
+//     const div = document.createElement('div.table');
+//     let tableHeaders = ['Business', 'Address', 'Phone', 'Website', 'Membership'];
+//     const createTableHead = () => {
+//         while(div.firstChild) div.removeChild(div.firstChild);
+//         let directoryTable = document.createElement('table');
+//         directoryTable.className = 'directoryTable';
+//         let directoryTableHead = document.createElement('thead');
+//         directoryTableHead.className = 'directoryTableHead';
+//         let tableHeaderRow = document.createElement('tr');
+//         tableHeaderRow.className = 'tableHeaderRow';
+
+//         tableHeaders.forEach(header => {
+//             let tableHeader = document.createElement('th');
+//             tableHeader.innerHTML = header;
+//             tableHeaderRow.append(tableHeader);
+//         })
+//         directoryTableHead.append(tableHeaderRow);
+//         directoryTable.append(directoryTableHead);
+
+//         let tableBody = document.createElement('tbody');
+//         tableBody.className = 'tableBody';
+//         directoryTable.append(tableBody);
+
+//         div.append(directoryTable);
+//  }    
+
+    data.companies.forEach((element) => {
+        let member = document.createElement('h2');
+        let company = document.createElement('p');
+        let street = document.createElement('p');
+        let phone = document.createElement('p');;
+        let url = document.createElement('p');
+        let membership = document.createElement('p');  
+                              
+        
+        company.textContent = element.name;
+        street.textContent = `${element.address[0].street} ${element.address[0].city}, ${element.address[0].state} ${element.address[0].zip}`; 
+        phone.textContent = `Phone # ${element.phone}`
+        url.textContent = element.website;        
+        membership.textContent = `Membership: ${element.membership}`;    
+
+        member.appendChild(company);
+        member.appendChild(street);
+        member.appendChild(phone);
+        member.appendChild(url);
+        member.appendChild(membership);
+        
+        gridView.appendChild(member);
+    }
+    )
+}
+
+
+const gridbutton = document.querySelector("#dir-grid");
+const listbutton = document.querySelector(".dir-grid");
+
+
+gridbutton.addEventListener("click", () => {
+	listbutton.classList.toggle("dir-list");
+	listbutton.classList.toggle("dir-grid");
+    gridView.innerHTML = '';
+    if(listbutton.classList.value == 'dir-list') {
+        getDirectory(displayListMembers);
+    }else {
+        getDirectory(displayGridMembers);
+    };
+});
+
