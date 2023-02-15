506195bc67596b5fd885a81ce47f3be18f289b24
hal22036 <jeni.lee.hales@gmail.com>
2023-02-15 13:13:56 -0800

    index images edit

--- chamber/scripts/chamber-weather.js
+++ chamber/scripts/chamber-weather.js
@@ -1,9 +1,9 @@
-const weather = document.querySelector('#weather');
+const weather = document.querySelector('#weather2');
 
 const myLat = "36.29027911329074";
 const myLon = "-115.15741454610898";
 const myKey = "a558cfbf5677c3eea632226cd7929c92";
-const myUrl = `//api.openweathermap.org/data/2.5/forecast?lat=${myLat}&lon=${myLon}&units=imperial&APPID=${myKey}`;
+const myUrl = `//api.openweathermap.org/data/2.5/forecast?lat=${myLat}&lon=${myLon}&cnt=25&units=imperial&APPID=${myKey}`;
 const gmtOffset = 8;
 
 
@@ -26,42 +26,35 @@ apiFetch();
 
 function displayResults(data) {
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday","Saturday"];
-  let dayCount = 0;
-  let pst = new Date();
-  let thisDay = pst.getDay();
+  let forecastDay = null;
+  let gmt = null;
 
-  for(var i=0; i< data.list.length; i++) {
-    gmt= new Date(data.list[i+4].dt_txt);
-    nextDay = new Date(gmt.setHours(gmt.getHours() - gmtOffset)).getDay();
-    if((thisDay != nextDay) || (i==0) || i == data.list.length - 1){
-      thisDay = nextDay;
-      dayCount += 1;
-      const weatherIcon = document.createElement('img');
-      const iconsrc = `https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png`;
-      const weatherSection = document.createElement("div");
-      weatherSection.classList.add("weather-section");
-      const dayOfWeek = document.createElement("h4");
-      dayOfWeek.classList.add('day-of-week');
-      const description = document.createElement("p");
+  for (var i=0; i<data.list.length; i+=8){
+    const iconsrc = `https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png`;
+    let desc = data.list[i].weather[0].description;
+    const weatherSection = document.createElement("div");
+    weatherSection.classList.add("weather-section");
+    const dayOfWeek = document.createElement("h4");
+    dayOfWeek.classList.add('day-of-week');
+    const description = document.createElement("p");
+    const weatherIcon = document.createElement('img');
 
-      let desc = data.list[i].weather[0].description;
       if(i==0){
         dayOfWeek.innerText=`Current:`;
       }else {
-        dayOfWeek.innerText=`${days[thisDay]}:`;
+        gmt= new Date(data.list[i].dt_txt);
+        forecastDay = new Date(gmt.setHours(gmt.getHours() - gmtOffset)).getDay();        
+        dayOfWeek.innerText=`${days[forecastDay]}:`;
       }      
-      weatherIcon.setAttribute('src', iconsrc);
-      weatherIcon.setAttribute('alt', desc);
-      description.innerHTML = `${desc} and ${data.list[i].main.temp}&deg;F`;   
-      weatherSection.appendChild(weatherIcon);
-      weatherSection.appendChild(description);
-      weather.appendChild(dayOfWeek);
-      weather.appendChild(weatherSection);
-      if(dayCount>= 4){
-        break;
-      }else {
 
-      };
-    };
-    }}
-  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    weatherIcon.setAttribute('width', '50');
+    weatherIcon.setAttribute('height', '50');
+    description.innerHTML = `${desc} and ${data.list[i].main.temp}&deg;F`;   
+    weatherSection.appendChild(weatherIcon);
+    weatherSection.appendChild(description);
+    weather.appendChild(dayOfWeek);
+    weather.appendChild(weatherSection);
+  }
+}
